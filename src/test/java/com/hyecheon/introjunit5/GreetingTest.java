6759d71f08943b72e3a05fe88331818d9fc6794f
hyecheon <rainbow880616@gmail.com>
2019-12-14 19:41:48 +0900

    Initial commit

---
+++ src/test/java/com/hyecheon/introjunit5/GreetingTest.java
@@ -0,0 +1,29 @@
+package com.hyecheon.introjunit5;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GreetingTest {
+
+    private Greeting greeting;
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("In Before Each...");
+        greeting = new Greeting();
+    }
+
+    @Test
+    void helloWorld() {
+        System.out.println(greeting.helloWorld());
+    }
+
+    @Test
+    void testHelloWorld() {
+        System.out.println(greeting.helloWorld("John"));
+    }
+    
+}
