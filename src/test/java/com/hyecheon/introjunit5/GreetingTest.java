1c360b152665a1d8cfdf58cba908d57c85c551de
hyecheon <rainbow880616@gmail.com>
2019-12-14 19:46:53 +0900

    Junit5 Test Instance LifeCycle Demo

--- src/test/java/com/hyecheon/introjunit5/GreetingTest.java
+++ src/test/java/com/hyecheon/introjunit5/GreetingTest.java
@@ -1,14 +1,17 @@
 package com.hyecheon.introjunit5;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
 
     private Greeting greeting;
+
+    @BeforeAll
+    public static void beforeClass() {
+        System.out.println("Before - I am only called Once!!");
+    }
 
     @BeforeEach
     void setUp() {
@@ -22,8 +25,22 @@ class GreetingTest {
     }
 
     @Test
+    void helloWorld2() {
+        System.out.println(greeting.helloWorld("Sam"));
+    }
+
+    @Test
     void testHelloWorld() {
         System.out.println(greeting.helloWorld("John"));
     }
 
+    @AfterEach
+    void tearDown() {
+        System.out.println("In After Each .....");
+    }
+
+    @AfterAll
+    public static void afterClass() {
+        System.out.println("After - I am only called Once!!");
+    }
 }
