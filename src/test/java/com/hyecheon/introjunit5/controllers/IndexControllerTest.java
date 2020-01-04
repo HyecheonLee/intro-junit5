36717d5efd2410ac4d14eb3e20a417c310df2e49
hyecheon <rainbow880616@gmail.com>
2020-01-04 14:56:28 +0900

    Junit Test Display Names

--- src/test/java/com/hyecheon/introjunit5/controllers/IndexControllerTest.java
+++ src/test/java/com/hyecheon/introjunit5/controllers/IndexControllerTest.java
@@ -2,6 +2,7 @@ package com.hyecheon.introjunit5.controllers;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,6 +16,7 @@ class IndexControllerTest {
         controller = new IndexController();
     }
 
+    @DisplayName("테스트 한다 적절한 뷰 이름이 리턴 된다 인덱스 페이지 에서 ")
     @Test
     void index() {
         assertEquals("index", controller.index());
@@ -23,6 +25,7 @@ class IndexControllerTest {
 
     }
 
+    @DisplayName("예외 테스트")
     @Test
     void oupsHandler() {
         assertTrue("notimplemented".equals(controller.oupsHandler()), () -> "This is some expensive Message to build for my test");
