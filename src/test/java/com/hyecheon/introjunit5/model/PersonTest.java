568862a0c65d8ec299a30cccade5c83adc89aee9
hyecheon <rainbow880616@gmail.com>
2020-01-06 21:36:58 +0900

    Repeating Tests with Junit5

--- src/test/java/com/hyecheon/introjunit5/model/PersonTest.java
+++ src/test/java/com/hyecheon/introjunit5/model/PersonTest.java
@@ -1,6 +1,8 @@
 package com.hyecheon.introjunit5.model;
 
 import com.hyecheon.introjunit5.ModelTests;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -27,5 +29,12 @@ class PersonTest implements ModelTests {
                 () -> assertEquals(person.getFirstName(), "Joe", "First Name Failed"),
                 () -> assertEquals(person.getLastName(), "Buck", "Last Name Failed"));
 
+    }
+
+    @RepeatedTest(value = 10, name = "{displayName} : {currentRepetition} - {totalRepetitions}")
+    @DisplayName("My Repeated Test")
+    @Test
+    void myRepeatedTest() {
+        //todo - impl
     }
 }
