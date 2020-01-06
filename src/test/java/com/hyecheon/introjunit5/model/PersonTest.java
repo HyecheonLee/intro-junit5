7a8c3616edd5efff49e12f3f59a4524b433180c4
hyecheon <rainbow880616@gmail.com>
2020-01-06 21:43:02 +0900

    JUnit Test Dependency Injection

--- src/test/java/com/hyecheon/introjunit5/model/PersonTest.java
+++ src/test/java/com/hyecheon/introjunit5/model/PersonTest.java
@@ -1,10 +1,7 @@
 package com.hyecheon.introjunit5.model;
 
 import com.hyecheon.introjunit5.ModelTests;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -36,5 +33,10 @@ class PersonTest implements ModelTests {
     @Test
     void myRepeatedTest() {
         //todo - impl
+    }
+
+    @RepeatedTest(5)
+    void myRepeatedTestWithDI(TestInfo testInfo, RepetitionInfo repetitionInfo) {
+        System.out.println(testInfo.getDisplayName() + ":" + repetitionInfo.getCurrentRepetition());
     }
 }
