7f43a857a8bebc8b324ca6877d3cb1702de17d14
hyecheon <rainbow880616@gmail.com>
2020-01-06 21:59:11 +0900

    Assignment Review

--- src/test/java/com/hyecheon/introjunit5/ModelTests.java
+++ src/test/java/com/hyecheon/introjunit5/ModelTests.java
@@ -1,7 +1,12 @@
 package com.hyecheon.introjunit5;
 
-import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Tag("model")
 public interface ModelTests {
+    @BeforeEach
+    default void beforeEachConsoleOutputer(TestInfo testInfo) {
+        System.out.println("Running Test - " + testInfo.getDisplayName());
+    }
 }
---
+++ src/test/java/com/hyecheon/introjunit5/model/PersonRepeatedTests.java
@@ -0,0 +1,26 @@
+package com.hyecheon.introjunit5.model;
+
+import com.hyecheon.introjunit5.ModelRepeatedTests;
+import org.junit.jupiter.api.*;
+
+@Tag("repeated")
+public class PersonRepeatedTests implements ModelRepeatedTests {
+    @RepeatedTest(value = 10, name = "{displayName} : {currentRepetition} - {totalRepetitions}")
+    @DisplayName("My Repeated Test")
+    @Test
+    void myRepeatedTest() {
+        //todo - impl
+    }
+
+    @RepeatedTest(5)
+    void myRepeatedTestWithDI(TestInfo testInfo, RepetitionInfo repetitionInfo) {
+        System.out.println(testInfo.getDisplayName() + ":" + repetitionInfo.getCurrentRepetition());
+    }
+
+    @RepeatedTest(value = 5, name = "{displayName} : {currentRepetition} | {totalRepetitions}")
+    @DisplayName("My Assignment Repeated Test")
+    @Test
+    void myAssignmentRepeated() {
+        //todo impl
+    }
+}
