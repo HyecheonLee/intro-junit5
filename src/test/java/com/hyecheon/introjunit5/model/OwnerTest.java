2dd7055c5409037ba0a9a611918e75b254de85fa
hyecheon <rainbow880616@gmail.com>
2020-01-07 22:19:29 +0900

    Junit Parameterized Test - Method Provider

--- src/test/java/com/hyecheon/introjunit5/model/OwnerTest.java
+++ src/test/java/com/hyecheon/introjunit5/model/OwnerTest.java
@@ -4,10 +4,9 @@ import com.hyecheon.introjunit5.ModelTests;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.stream.Stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -65,4 +64,17 @@ class OwnerTest implements ModelTests {
         System.out.println(stateName + " = " + val1 + " : " + val2);
     }
 
+    @DisplayName("Method Provider Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @MethodSource("getArgs")
+    void fromMethodTest(String stateName, int val1, int val2) {
+        System.out.println(stateName + " = " + val1 + " : " + val2);
+    }
+
+    static Stream<Arguments> getArgs() {
+        return Stream.of(
+                Arguments.of("FL", 5, 1),
+                Arguments.of("OH", 2, 8),
+                Arguments.of("MI", 3, 5));
+    }
 }
