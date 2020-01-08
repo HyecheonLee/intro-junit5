80ce48db1e29afb28a4ae7f6d6a64629ae717817
hyecheon <rainbow880616@gmail.com>
2020-01-08 23:19:20 +0900

    Verify Interactions With Mockito Mocks

--- src/test/java/com/hyecheon/introjunit5/service/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/com/hyecheon/introjunit5/service/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -1,6 +1,11 @@
 package com.hyecheon.introjunit5.service.springdatajpa;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.atMost;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import com.hyecheon.introjunit5.model.Speciality;
 import com.hyecheon.introjunit5.repositories.SpecialtyRepository;
@@ -21,12 +26,44 @@ class SpecialitySDJpaServiceTest {
   SpecialitySDJpaService service;
 
   @Test
-  void delete() {
+  void deleteById() {
     service.deleteById(1L);
+    service.deleteById(1L);
+
+    verify(specialtyRepository, times(2)).deleteById(1L);
+  }
+
+  @Test
+  void deleteByIdAtLest() {
+    service.deleteById(1L);
+    service.deleteById(1L);
+
+    verify(specialtyRepository, atLeastOnce()).deleteById(1L);
+  }
+
+  @Test
+  void deleteByIdAtMost() {
+    service.deleteById(1L);
+    service.deleteById(1L);
+
+    verify(specialtyRepository, atMost(5)).deleteById(1L);
+  }
+
+  @Test
+  void deleteByIdAtNever() {
+    service.deleteById(1L);
+    service.deleteById(1L);
+
+    verify(specialtyRepository, atLeastOnce()).deleteById(1L);
+
+    verify(specialtyRepository, never()).deleteById(5L);
   }
 
   @Test
   void testDelete() {
     service.delete(new Speciality());
+    service.delete(new Speciality());
+
+
   }
 }
